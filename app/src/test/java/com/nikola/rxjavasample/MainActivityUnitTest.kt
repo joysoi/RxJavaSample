32c8defffb909a604f24354d22c8583baa41a868
Nikola Nikolovski <nikola.joysoi@gmail.com>
2022-05-20 11:11:36 -0400

    Update MainActivityUnitTest.kt
    Added test case for Observable of type Observable

--- app/src/test/java/com/nikola/rxjavasample/MainActivityUnitTest.kt
+++ app/src/test/java/com/nikola/rxjavasample/MainActivityUnitTest.kt
@@ -1,6 +1,7 @@
 package com.nikola.rxjavasample
 
 import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Observable
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -19,6 +20,15 @@ class MainActivityUnitTest {
     @Test
     fun testHelloWorldRx(){
         Flowable.just("Hello World").subscribe{ item->
+            println(item)
+        }
+
+
+    }
+    
+     @Test
+    fun testHelloWorldRxSingle(){
+        Observable.just("Hello World").subscribe{ item->
             println(item)
         }
 
